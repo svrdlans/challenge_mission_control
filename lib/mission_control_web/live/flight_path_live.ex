4c6c5cddfe2e5b620afa21abee251f08e3d21d3e
Srđan Svrdlan <srdjan.svrdlan@vibe.rs>
2025-11-24 08:54:44 +0100

    Connect fuel calculation to live view

--- lib/mission_control_web/live/flight_path_live.ex
+++ lib/mission_control_web/live/flight_path_live.ex
@@ -38,15 +38,17 @@ defmodule MissionControlWeb.FlightPathLive do
         {:error, error} -> {new_step(param_action, param_planet), [error | errors]}
       end
 
-    total_fuel =
-      if mass && flight_path != [],
-        # FlightPaths.calculate_fuel_for_flight_path(flight_path, mass),
-        do: 1111,
-        else: nil
+    {total_fuel, errors} =
+      if mass && flight_path != [] do
+        case FlightPaths.calculate_fuel_for_flight_path(flight_path, mass) do
+          {:ok, total_fuel} -> {total_fuel, errors}
+          {:error, error} -> {nil, [error | errors]}
+        end
+      else
+        {nil, errors}
+      end
 
     insert_disabled = param_action == "" or param_planet == ""
-
-    IO.inspect(errors, label: "ERR")
 
     socket =
       assign(socket,
@@ -57,8 +59,6 @@ defmodule MissionControlWeb.FlightPathLive do
         errors: Enum.reverse(errors)
       )
 
-    IO.inspect(socket.assigns, label: "VAL")
-
     {:noreply, socket}
   end
 
@@ -67,21 +67,34 @@ defmodule MissionControlWeb.FlightPathLive do
 
     flight_path_step = to_flight_path_step(step)
     flight_path = flight_path ++ [flight_path_step]
+    total_fuel = recalculate_total_fuel(flight_path, mass)
 
-    socket = assign(socket, flight_path: flight_path, total_fuel: 2222)
+    socket = assign(socket, flight_path: flight_path, total_fuel: total_fuel)
 
     {:noreply, socket}
   end
 
   def handle_event("remove_from_flight_path", %{"index" => index}, socket) do
     id = String.to_integer(index)
+    %{flight_path: flight_path, mass: mass} = socket.assigns
 
     flight_path =
-      socket.assigns.flight_path
+      flight_path
       |> Enum.reject(&(&1.id == id))
 
-    socket = assign(socket, flight_path: flight_path)
+    total_fuel = recalculate_total_fuel(flight_path, mass)
+
+    socket = assign(socket, flight_path: flight_path, total_fuel: total_fuel)
     {:noreply, socket}
+  end
+
+  defp recalculate_total_fuel(flight_path, mass) do
+    with {:ok, mass} <- parse_mass(mass),
+         {:ok, total_fuel} <- FlightPaths.calculate_fuel_for_flight_path(flight_path, mass) do
+      total_fuel
+    else
+      _ -> nil
+    end
   end
 
   defp to_flight_path_step(%{action: action, planet: planet}) do
@@ -97,13 +110,18 @@ defmodule MissionControlWeb.FlightPathLive do
 
   defp parse_mass(""), do: {:error, "Mass is required"}
 
-  defp parse_mass(mass_str) do
-    case Float.parse(mass_str) do
+  defp parse_mass(mass_str) when is_binary(mass_str) do
+    case Integer.parse(mass_str) do
       {mass, ""} when mass > 0 -> {:ok, mass}
       {_mass, ""} -> {:error, "Mass must be positive"}
       _ -> {:error, "Mass must be a number greater than 0"}
     end
   end
+
+  defp parse_mass(mass) when is_integer(mass) and mass > 0,
+    do: {:ok, mass}
+
+  defp parse_mass(_), do: {:error, "Mass must be a number greater than 0"}
 
   def parse_step(action, planet) when action == "" or planet == "",
     do: {:error, "Both action and planet must be selected"}
